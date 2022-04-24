8f655cd7a4a65d62001402b8a546334cec88aa27
Magnumsrt <magnumsrtrblx@gmail.com>
2022-04-24 14:43:40 -0400

    brrrrr

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -165,7 +165,7 @@ class TitleState extends MusicBeatState
 
 		logoBl = new FlxSprite(-150, -100);
 		#if MODS
-		if (FileSystem.exists(Paths.image('logoBumpin')) && FileSystem.exists(Paths.modIcon('logoBumpin')))
+		if (FileSystem.exists(Paths.image('assets/images/logoBumpin.png')) && FileSystem.exists(Paths.modIcon('logoBumpin')))
 		{
 			logoBl.frames = Paths.getModsSparrowAtlas('logoBumpin');
 			LoggingUtil.writeToLogFile('Found A Modded Logo!');
@@ -190,7 +190,7 @@ class TitleState extends MusicBeatState
 
 		#if MODS
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		if (FileSystem.exists(Paths.image('gfDanceTitle')) && FileSystem.exists(Paths.modIcon('gfDanceTitle')))
+		if (FileSystem.exists('assets/images/gfDanceTitle.png') && FileSystem.exists(Paths.modIcon('gfDanceTitle')))
 		{
 			gfDance.frames = Paths.getModsSparrowAtlas('gfDanceTitle');
 			LoggingUtil.writeToLogFile('Found A Modded Title Girlfriend!');
@@ -210,7 +210,7 @@ class TitleState extends MusicBeatState
 
 		#if MODS
 		titleText = new FlxSprite(150, FlxG.height * 0.8);
-		if (FileSystem.exists(Paths.image('titleEnter')) && FileSystem.exists(Paths.modIcon('titleEnter')))
+		if (FileSystem.exists(Paths.image('assets/images/titleEnter.png')) && FileSystem.exists(Paths.modIcon('titleEnter')))
 		{
 			titleText.frames = Paths.getModsSparrowAtlas('titleEnter');
 			LoggingUtil.writeToLogFile('Found A Modded Title Text!');
